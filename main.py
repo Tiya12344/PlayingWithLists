1a9b675fe767933f5caed65bd04e7f178ab8990a
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-03-08 18:22:23 +0000

    playing with lists

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+
+ListOfNumbers= [1, 4, 7, 1004, 8, 12, 19, 74, 63]
+NumberToFind = 99
+for i in ListOfNumbers:
+    if i == NumberToFind:
+        print(f"your number {NumberToFind} is present and in the list")
+else:
+    print(f"your number {NumberToFind} is not  present and in the list")
