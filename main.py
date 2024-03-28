8741f03570669dff4ec30263ff355afc24131388
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-03-28 17:22:07 +0000

    we created a list

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ for i in ListOfNumbers:
         print(f"your number {NumberToFind} is present and in the list")
 else:
     print(f"your number {NumberToFind} is not  present and in the list")
+ListOfNumbers =[123456789, 21, 7, 277, 12, 133, 1267, 89, 69, 43,6, 188]
+print(list(range(0,7)))
+genre = ["pop", "rock", "jazz", "romance"]
+for i in range (len(genre)):
+    print(f"i like {genre[i]}")
